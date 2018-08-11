979f5e0259d9cfc3ce3c10dd4402446b56b20926
vivgeo <vivek.george@ryerson.ca>
2018-08-11 19:56:16 -0400

    oh god it works now

--- js/clip.js
+++ js/clip.js
@@ -1,5 +1,9 @@
 var clip = new ClipboardJS('.btn');
 
-clip.on('success', function(e) {
-    alert('copy')
+clip.on('success', function(event) {
+    event.clearSelection();
+    event.trigger.textContent = 'Copied';
+    window.setTimeout(function() {
+        event.trigger.textContent = 'Copy';
+    }, 2000);
 });
